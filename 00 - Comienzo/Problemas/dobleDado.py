ed076474c35380fa4363c2e44556845e1a4ebe4d
Marcos Martinez <zerobananaboy@gmail.com>
2025-09-19 14:14:19 +0200

    Cambios en el ejercicio de los 2 dados

--- 00 - Comienzo/Problemas/dobleDado.py
+++ 00 - Comienzo/Problemas/dobleDado.py
@@ -1,14 +1,20 @@
 import random
 
-contador = 1
+contador = 0
 
-i = random.randint(1,6)
-j = random.randint(1,6)
-print(i," - ",j, end="\n")
+# dado1 = random.randint(1, 6)
+# dado2 = random.randint(1, 6)
+iguales = False
 
-while (i) != (j):
-    i=random.randint(1,6)
-    j=random.randint(1,6)
-    print(i, " - ", j, end="\n")
+# print(dado1, " - ", dado2, end="\n")
+
+# while (dado1) != (dado2):
+
+while iguales == False:
+    dado1 = random.randint(1, 6)
+    dado2 = random.randint(1, 6)
+    if dado1 == dado2:
+        iguales = True
+    print(dado1, " - ", dado2, end="\n")
     contador += 1
 print("Veces que se han tirado: ",contador)

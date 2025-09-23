3f443b2d76dfae72da1f13573839c6627fec32b0
Marcos Martinez <zerobananaboy@gmail.com>
2025-09-23 10:03:27 +0200

    Ejercicios1-10 del boletin

---
+++ 00 - Comienzo/Problemas/boletin1/Ejercicios1-10.py
@@ -0,0 +1,60 @@
+import random
+
+# Ejercicio 1
+# i = 0
+# for i in range(1,11):
+#     print(i)
+
+
+# Ejercicio 1
+# i = 0
+# cont = 0
+# while cont < 50:
+#     if not i%2:
+#         print(i)
+#         cont= cont + 1
+#     i=i+1
+
+
+# Ejercicio 3
+# i = int(input("Introduce un numero\n"))
+# j = 1
+# for j in range (1,6):
+#     print(i*j)
+
+# Ejercicio 4
+# i = 10000
+# for i in range(10000,0,-1):
+#     if i%7 == 0:
+#         print(i)
+
+# Ejercicio 5
+# i = int(input("Introduce un numero:\n"))
+#
+# if i%2 == 0:
+#     print("Es par")
+# else:
+#     print("Es impar")
+
+# Ejercicio 6
+# i = int(input("Introduce un numero:\n"))
+#
+# if i%3 == 0:
+#     print("Es divisible por 3")
+# else:
+#     print("No es divsible por 3")
+
+# Ejercicio 7
+# i = int(input("Introduce le precio del producto:\n"))
+# print("El precio tras iva es: ",i*1.21)
+
+# Ejercicio 8
+# importe = float(input("Introduce un importe:\n"))
+# meses = float(input("Introduce un plazo:\n"))
+# print("Tienes que pagar",round(importe/meses,2),"€ cada mes")
+
+# Ejercicio 9
+# print(random.randint(1,50))
+
+# Ejercicio 10
+# print(random.randint(1,6),"-",random.randint(1,6))

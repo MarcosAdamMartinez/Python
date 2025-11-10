cba5a20a54ffb274105e52fb393500fb106eb4ee
bananaboy448 <zerobananaboy@gmail.com>
2025-11-10 23:40:48 +0100

    Reorganizacion

--- EjerciciosCasa/procesadorDeNumeros.py
+++
@@ -1,9 +0,0 @@
-line = input("Ingresa una línea de números, sepáralos con espacios: ")
-strings = line.split()
-total = 0
-try:
-    for substr in strings:
-        total += float(substr)
-    print("El total es:", total)
-except:
-    print(substr, "no es un numero.")
